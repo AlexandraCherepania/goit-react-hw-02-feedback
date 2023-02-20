02fcb21bf3d8b91cbbcf7295d8717fc3f0b426e2
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2023-02-20 19:34:51 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
-export const App = () => {
-  return (
+import React, { Component } from 'react'
+
+
+class App extends Component {
+  render() {
+    return (
     <div
       style={{
         height: '100vh',
@@ -10,7 +14,33 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template 2
+    Міша Черепаня
+
     </div>
   );
-};
+  }
+
+}
+export default App;
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//     Міша Черепаня
+
+//     </div>
+//   );
+// };
