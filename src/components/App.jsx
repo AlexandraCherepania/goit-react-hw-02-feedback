cc030f52f626886f6c269cbd21233427c2fba7de
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2023-02-21 13:48:50 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,79 @@
 import React, { Component } from 'react'
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics'
+import { Feedback } from "./Feedback/Feedback";
+import {Notification} from "./Notification/Notification"
 
+class App extends Component { 
 
-class App extends Component {
-  render() {
-    return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    Міша Черепаня
-
-    </div>
-  );
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
   }
 
+  handleIncrement = (e) => {
+    this.setState((prevState) => ({
+      
+      [e.target.name]: prevState[e.target.name] + 1,
+
+       
+    }))
+    console.log("Console: ", e.target.name)
+  };
+  
+  countTotalFeedback = () => {
+    const {good, neutral, bad } =  this.state
+  
+    return good + neutral + bad
+    
+  }
+
+  handltPositive = () => {
+    const {good, neutral, bad } =  this.state
+    return (good * 100 / (good + neutral + bad)).toFixed(2)
+  }
+
+  render() {
+    const { good , neutral , bad} = this.state
+  
+      return <div
+      style={{
+      textAlign: 'center',
+      fontSize: 24,
+      color: '#010101'
+      }}>
+       
+
+        <Section title = "Please leave your feedback">
+          <Feedback
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleIncrement}>
+            
+            </Feedback>
+       </Section>
+     
+
+         
+        <Section title='Statistick'>
+          {this.countTotalFeedback() > 0
+          ? <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.handltPositive()}>
+          </Statistics>
+          : <Notification message='There is no feedback' />}
+           </Section>
+         
+        </div>
+       
+     
+   }
 }
+
 export default App;
 
 
 
-
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//     Міша Черепаня
-
-//     </div>
-//   );
-// };
